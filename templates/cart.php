3c866014de4d803ecf5577ac9b0e08bd0932d589
Erwan Leboucher <erwanleboucher@gmail.com>
2018-03-31 22:44:26 +0200

    handles product and cart

--- templates/cart.php
+++ templates/cart.php
@@ -5,25 +5,25 @@
     function remove_prod($id)
     {
         echo $id."\n";
-        $index = array_search($id, array_column($_SESSION["products"], 'id'));
+        $index = array_search($id, array_column($_SESSION["products"], "id"));
         echo $index."\n";
         print_r ($_SESSION["products"]);
         if ($index !== false)
             array_splice($_SESSION["products"], $index, 1);
     }
     
-    if (isset($_GET["del"]) && is_numeric($_GET["del"])){
-        remove_prod($_GET["del"]);
+    if (isset($_POST["del"]) && is_numeric($_POST["del"])){
+        remove_prod($_POST["del"]);
     }
-    if (isset($_GET["quantity"]) && isset($_GET["id"]) && is_numeric($_GET["quantity"]) && is_numeric($_GET["id"])) {
-        $index = array_search($_GET["id"], array_column($_SESSION["products"], 'id'));
-        $_SESSION["products"][$index]["quantity"] = $_GET["quantity"];
+    if (isset($_POST["quantity"]) && isset($_POST["id"]) && is_numeric($_POST["quantity"]) && is_numeric($_POST["id"])) {
+        $index = array_search($_POST["id"], array_column($_SESSION["products"], "id"));
+        $_SESSION["products"][$index]["quantity"] = $_POST["quantity"];
     }
-    $_SESSION["products"] = array(array("id" => 1, "quantity" => 1 ), array("id" => 2, "quantity" => 2 ), array("id" => 6, "quantity" => 4 ));
+    //$_SESSION["products"] = array(array("id" => 1, "quantity" => 1 ), array("id" => 2, "quantity" => 2 ), array("id" => 6, "quantity" => 4 ));
     if (isset($_SESSION["products"]))
     {
         foreach ($_SESSION["products"] as $product => $id) {
-            $ret = query("SELECT * FROM `product` where ID = {$id["id"]}");
+            $ret = query("SELECT * FROM `product` where ID = '$id[id]'");
             if (mysqli_num_rows($ret) > 0) {
                 
                 while($row = mysqli_fetch_assoc($ret)) {
@@ -38,11 +38,11 @@
                         </div>
                        
                         <div class="button">
-                            <form type="post" class="form1">
-                                <input type="number" name="quantity" min="1" max="$row[stock]" value="1">
+                            <form method="post" class="form1">
+                                <input type="number" name="quantity" min="1" max="$row[stock]" value="$id[quantity]"">
                                 <button type="submit" name="quantity" value="$row[id]">Modifier</button>
                             </form>
-                            <form type="post" class="form2">
+                            <form method="post" class="form2">
                             <button type="submit" name="del" value="$id[id]"/>Supprimer</button>
                             </form>  
                         </div>
--- templates/products.php
+++ templates/products.php
@@ -1,33 +1,38 @@
 <?php
+    session_start();
+    if (isset($_POST["add"]) && is_numeric($_POST["add"]) && isset($_POST["quantity"]) && is_numeric($_POST["quantity"])){
+        $index = array_search($_POST["add"], array_column($_SESSION["products"], "id"));
+        if ($index === false)
+            $_SESSION["products"][] = array("id" => $_POST["add"], "quantity" => $_POST["quantity"]);
+        else
+        $_SESSION["products"][$index] = array("id" => $_POST["add"], "quantity" => $_POST["quantity"]);
+    }
     include ("ressources/handle_db.php");
 
-        $ret = query("SELECT * FROM `product`");
-        
-        if (mysqli_num_rows($ret) > 0) {
-            while($row = mysqli_fetch_assoc($ret)) {
-                echo <<<EOL
-                    <div class="item">
-                        <div class="image">
-                            <img src="$row[img]" alt="$row[name]"></img>
-                        </div>
-                        <div class="description">
-                            <span>$row[name]</span>
-                            <span>$row[description]</span>
-                        </div>
-                       
-                        <div class="button">
-                            <form type="post" class="form1">
-                                <input type="number" name="quantity" min="1" max="$row[stock]" value="1">
-                                <button type="submit" name="quantity" value="$row[id]">Modifier</button>
-                            </form>
-                            <form type="post" class="form2">
-                                <button type="submit" name="add" value="$row[id]" class="buy"/>Commander</button>
-                            </form>  
-                        </div>
-                        <div class="total-price">$row[price]$</div> 
+    $ret = query("SELECT * FROM `product`");
+    
+    if (mysqli_num_rows($ret) > 0) {
+        while($row = mysqli_fetch_assoc($ret)) {
+            echo <<<EOL
+                <div class="item">
+                    <div class="image">
+                        <img src="$row[img]" alt="$row[name]"></img>
                     </div>
+                    <div class="description">
+                        <span>$row[name]</span>
+                        <span>$row[description]</span>
+                    </div>
+                
+                    <div class="button">
+                        <form method="post" class="form1">
+                            <input type="number" name="quantity" min="1" max="$row[stock]" value="1">
+                            <button type="submit" name="add" value="$row[id]" class="buy"/>Commander</button>
+                        </form>  
+                    </div>
+                    <div class="total-price">$row[price]$</div> 
+                </div>
 EOL;
-            }  
-        }
+        }  
+    }
 
 ?>
