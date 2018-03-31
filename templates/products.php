5e903b5d21f89d7c39272d7abb5e267501c02242
Erwan LEBOUCHER <elebouch@e1r2p13.42.fr>
2018-03-31 10:04:09 +0200

    add date and display product

---
+++ templates/products.php
@@ -0,0 +1,21 @@
+<?php
+    include ("../handle_db.php");
+    $ret = query("SELECT * FROM `product`");
+    
+    if (mysqli_num_rows($ret) > 0) {
+        echo "<ul>";
+        while($row = mysqli_fetch_assoc($ret)) {
+            echo <<<EOL
+                <li>
+                    <img src="$row[img]"></img>
+                    <h5>$row[name]</h5>
+                    <p>$row[description]</p>
+                    <p>$row[price]<p>
+                    <p>$row[stock]<p>
+                </li>
+EOL;
+        }
+        echo "<ul>";    
+    }
+
+?>
