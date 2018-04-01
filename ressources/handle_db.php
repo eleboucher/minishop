f4a0dab6e79e05d02872c93ffcdfa8f5c9c1f6c5
Benjamin MUSELET <bmuselet@e1r3p12.42.fr>
2018-04-01 15:07:00 +0200

    mdp

--- ressources/handle_db.php
+++ ressources/handle_db.php
@@ -1,6 +1,6 @@
 <?php
     function query($query){
-        $mysqli = mysqli_connect("localhost", "root", "azerty", "minishop");
+        $mysqli = mysqli_connect("localhost", "root", "berni196742", "minishop");
         if ($mysqli){
             $res = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
         }
