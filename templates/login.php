3751dafef04d45555295c829fa7807cb3883f1ab
Erwan LEBOUCHER <elebouch@e1r4p12.42.fr>
2018-04-01 14:55:46 +0200

    fix login and logout

--- templates/login.php
+++ templates/login.php
@@ -8,10 +8,11 @@ function connexion($email, $hash_passwd)
 {  
   $var1 = query("SELECT passwd FROM `user` WHERE email = '$email'");
   $var2 = query("SELECT email, id FROM `user` WHERE passwd = '$hash_passwd'");
-  if (isset($var1) && mysqli_num_rows($var1) > 0)
+  if (mysqli_num_rows($var1) > 0)
     $ret1 = mysqli_fetch_assoc($var1);
-  if (isset($var2) && mysqli_num_rows($var2) > 0)
+  if (mysqli_num_rows($var2) > 0)
     $ret2 = mysqli_fetch_assoc($var2);
+    echo"adfasf";
   if (isset($ret2['email']) && $ret2['email'] === $email && isset($ret1['passwd']) && $ret1['passwd'] === $hash_passwd)
     return ($ret2['id']);
   else
@@ -40,10 +41,10 @@ else if (isset($_POST['submit']) && $_POST['submit'] === "Se déconnecter")
     session_unset();
     session_destroy();
     echo "Vous êtes déconnecté.\n";
-    header("Location: index.php");
+    header("Location: ../index.php");
 }
 ?>
-
+<form method="post" id="connection">
     <fieldset>
         <h1>Se connecter</h1>
         <label for="email">E-mail : </label><input id="email" name="email"/><br>
@@ -51,6 +52,4 @@ else if (isset($_POST['submit']) && $_POST['submit'] === "Se déconnecter")
         <input type="submit" class="submit" name="submit" value="Login" ><br/>
     </fieldset>
 </form>
-<form method="post" id="connection">
-    <input type="submit" class="submit" name= "submit" value="Se déconnecter"/>
- </form>
+
--- whoami.php
+++ whoami.php
@@ -7,6 +7,9 @@
   <?php include ("templates/navbar.php"); ?>
     <div class="section">
       <?php include ("templates/whoami.php"); ?>
+      <form method="post" action="templates/login.php" id="connection">
+          <input type="submit" class="submit" name= "submit" value="Se déconnecter"/>
+      </form>
       <?php include ("templates/order.php"); ?>
     </div>
 </body>
